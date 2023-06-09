2bedea2a4c38c93469cc26d025df988d95fcd6bc
Ahmet Bogar <ahmetbogar@hotmail.com>
2023-06-10 02:22:19 +0300

    Add project files.

---
+++ AkademiPlus.PresentationLayer/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiPlus.PresentationLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Deneme()
+        {
+            return Ok();
+        }
+
+    }
+}
