ceba95a17bb9828109e858bd55592aaaf391cd81
Ahmet Bogar <ahmetbogar@hotmail.com>
2023-06-11 02:23:43 +0300

    v1

--- AkademiPlusApi.ConsumeLayer/Controllers/CustomerController.cs
+++ AkademiPlusApi.ConsumeLayer/Controllers/CustomerController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
@@ -25,6 +26,47 @@ namespace AkademiPlusApi.ConsumeLayer.Controllers
             {
                 var jsonData=await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<CustomerListViewModel>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult>AddCustomer(CreateCustomerViewModel createCustomerViewModel)
+        {
+            var client=_httpClientFactory.CreateClient();
+            var jsonData=JsonConvert.SerializeObject(createCustomerViewModel);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44370/api/Customer", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:44370/api/Customer?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateCustomer(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44370/api/Customer/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<CustomerUpdateViewModel>(jsonData);
                 return View(values);
             }
             return View();
